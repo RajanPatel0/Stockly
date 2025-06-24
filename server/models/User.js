51163a951ce0c02b41636aa6ce15d827806cc862
Rajan Patel0 <rajanpatel.ias@gmail.com>
2025-06-24 17:26:26 +0530

    mainsetup in ESM

--- server/models/User.js
+++ server/models/User.js
@@ -1,24 +1,24 @@
 import mongoose from 'mongoose';
-import bcrypt from 'bcrypt';
-
+import bcrypt from 'bcryptjs';
 
 const userSchema = new mongoose.Schema({
-    name:{type:String, required:true},
-    email:{type:String, unique:true},
-    password:{type:String, required:true},
-    role:{type:String, enum:['vendor', 'user','admin'], default:'user'}  ,
+  name: { type: String, required: true },
+  email: { type: String, unique: true },
+  password: { type: String, required: true },
+  role: { type: String, enum: ['vendor', 'user', 'admin'], default: 'user' },
 });
 
-
-userSchema.pre('save', async function(next){
-    if(!this.isModified('password'))
-        return next();
-    this.password=await bcrypt.hash(this.password,8);
-    next();
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next();
+  this.password = await bcrypt.hash(this.password, 8);
+  next();
 });
 
 userSchema.methods.comparePassword = function (password) {
   return bcrypt.compare(password, this.password);
 };
 
-module.exports= mongoose.Model('User',userSchema);
+const User = mongoose.model('User', userSchema);
+
+// ✅ Use ES Module export
+export default User;
