6d25763aa97a6b728156616e2db0f885a1da1e76
Rajan Patel0 <rajanpatel.ias@gmail.com>
2025-07-02 22:57:42 +0530

    enhancing with merging different components in this Homepage required

---
+++ client/src/pages/HomePage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import SearchBanner from '../components/SearchBanner'
+import Category from '../components/Category'
+
+const HomePage = () => {
+  return (
+    <>
+      <SearchBanner />
+      <Category />
+    </>
+  )
+}
+
+export default HomePage
