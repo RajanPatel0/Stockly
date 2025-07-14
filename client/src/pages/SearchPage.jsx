d3f6b509520f26734c12ad634e14c7a530cef9a7
Rajan Patel0 <rajanpatel.ias@gmail.com>
2025-07-14 23:56:16 +0530

    product searching result format upgrade

--- client/src/pages/SearchPage.jsx
+++ client/src/pages/SearchPage.jsx
@@ -36,7 +36,7 @@ export default function SearchPage() {
             ) : searchResults.length === 0 && query ? (
               <p className="text-gray-400 text-center">No products found</p>
             ) : (
-              <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mt-6">
+              <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 mt-6">
                 {searchResults.map((product) => (
                   <ProductCard key={product._id} product={product} />
                 ))}
