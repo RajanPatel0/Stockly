fc661d64bc7599c00fb29380b5558bb077d50ca3
Rajan Patel0 <rajanpatel.ias@gmail.com>
2025-07-17 01:38:49 +0530

    creating vendor requied location and id fetching function route

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,11 +1,14 @@
 import express from 'express';
-import {register, login } from '../controllers/authController.js';
+import {register, login, getVendorById } from '../controllers/authController.js';
 
 const router = express.Router();
 
 router.post('/register', register);
 
 router.post('/login', login);
+
+router.get("/vendor/:id", getVendorById);
+
 
 export default router;
 
